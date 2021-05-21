397d385f37cc6adeda3534d5622298e67109d36c
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-05-20 23:24:23 -0700

    remove not needed icons

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -19,36 +19,37 @@ class MainScreen extends StatelessWidget {
                   // Tab(icon: Icon(Icons.directions_bike)),
                 ],
               ),
-              actions: <Widget>[
-                IconButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/Movies');
-                    },
-                    icon: Icon(Icons.album_sharp),
-                    tooltip: "Go to Movies Page"),
-                IconButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/TVShows');
-                    },
-                    icon: Icon(Icons.add_rounded),
-                    tooltip: "Go To TVShows Page"),
-                // IconButton(
-                //     onPressed: () {
-                //       Navigator.pushNamed(context, '/');
-                //     },
-                //     icon: Icon(Icons.exit_to_app_sharp),
-                //     tooltip: "Exit App"),
-                IconButton(
-                  icon: const Icon(Icons.add_alert),
-                  tooltip: 'Show Snackbar',
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text('ZZ Top \n Fandango \n Mexican Black Bird'),
-                      backgroundColor: Colors.purple,
-                    ));
-                  },
-                ),
-              ]),
+              // actions: <Widget>[
+              //   IconButton(
+              //       onPressed: () {
+              //         Navigator.pushNamed(context, '/Movies');
+              //       },
+              //       icon: Icon(Icons.album_sharp),
+              //       tooltip: "Go to Movies Page"),
+              //   IconButton(
+              //       onPressed: () {
+              //         Navigator.pushNamed(context, '/TVShows');
+              //       },
+              //       icon: Icon(Icons.add_rounded),
+              //       tooltip: "Go To TVShows Page"),
+              //   // IconButton(
+              //   //     onPressed: () {
+              //   //       Navigator.pushNamed(context, '/');
+              //   //     },
+              //   //     icon: Icon(Icons.exit_to_app_sharp),
+              //   //     tooltip: "Exit App"),
+              //   IconButton(
+              //     icon: const Icon(Icons.add_alert),
+              //     tooltip: 'Show Snackbar',
+              //     onPressed: () {
+              //       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              //         content: Text('ZZ Top \n Fandango \n Mexican Black Bird'),
+              //         backgroundColor: Colors.purple,
+              //       ));
+              //     },
+              //   ),
+              // ]
+            ),
         body: TabBarView(
           children: [
             Icon(Icons.directions_car),
