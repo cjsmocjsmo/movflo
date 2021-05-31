5d22540cda08e29bf7049175f511a3913b564c22
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-05-31 16:55:39 -0700

    building up tvshows

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'tvshows/startrektvcard.dart';
+
+
+import 'tvshows/alienworldscard.dart';
+import 'tvshows/alteredcarboncard.dart';
+import 'tvshows/discoverycard.dart';
+import 'tvshows/falconwintersoldiercard.dart';
 import 'tvshows/lowerdeckscard.dart';
+import 'tvshows/startrektvcard.dart';
+
 
 final List<String> movies = <String>[
   'Action',
@@ -153,120 +160,20 @@ Widget moviesListView = ListView.builder(
   }
 );
 
-// void starTrekNav(context) {
-//   Navigator.pushNamed(context, '/Star Trek TV');
-// }
-
 Widget tvShowsListView(BuildContext context) {
   return ListView(
   shrinkWrap: true,
   padding: const EdgeInsets.all(10.0),
   children: <Widget>[
-    StarTrekTVCard(),
+    AlienWorldsCard(),
+    AlteredCarbonCard(),
+    DiscoveryCard(),
+    FalconWinterSoldierCard(),
     LowerDecksCard(),
+    StarTrekTVCard(),
     
-    // Center(
-    //   child: Card(
-    //     color: Colors.purple[900],
-    //     child: InkWell(
-    //       splashColor: Colors.red.withAlpha(15),
-    //       onTap: () {
-    //         print('Card tapped.');
-    //       },
-    //       child: SizedBox(
-    //         width: 775.0,
-    //         height: 290.0,
-    //         child: Row(
-    //           children: <Widget>[
-    //             Image.asset(
-    //               'images/lowerdecks.jpg',
-    //               fit: BoxFit.contain,
-    //                 height: 355.5,
-    //                 width: 200.0,
-    //             ),
-    //             Expanded(
-    //               child: Column(
-    //               children: <Widget>[
-    //                 TextButton(
-    //                   child: const Text('Season 1'),
-    //                   style: TextButton.styleFrom(
-    //                     textStyle: TextStyle(fontSize: 32, color: Colors.white)
-    //                   ),
-    //                   onPressed: () {
-    //                     starTrekNav(context);
-    //                   },
-    //                 ),
-    //                 const SizedBox(width: 12),
-    //                 TextButton(
-    //                   child: const Text('Season 2'),
-    //                   style: TextButton.styleFrom(
-    //                     textStyle: TextStyle(fontSize: 32)
-    //                   ),
-    //                   onPressed: () {
-    //                     starTrekNav(context);
-    //                   },
-    //                 ),
-    //                 const SizedBox(width: 12),
-    //                 TextButton(
-    //                   child: const Text('Season 3'),
-    //                   style: TextButton.styleFrom(
-    //                     textStyle: TextStyle(fontSize: 32)
-    //                   ),
-    //                   onPressed: () {
-    //                     starTrekNav(context);
-    //                   },
-    //                 ),
-    //                 const SizedBox(width: 12),
-    //                 TextButton(
-    //                   child: const Text('Season 4'),
-    //                   style: TextButton.styleFrom(
-    //                     textStyle: TextStyle(fontSize: 32)
-    //                   ),
-    //                   onPressed: () {
-    //                     starTrekNav(context);
-    //                   },
-    //                 ),
-    //                 const SizedBox(width: 12),
-    //                 TextButton(
-    //                   child: const Text('Season 5'),
-    //                   style: TextButton.styleFrom(
-    //                     textStyle: TextStyle(fontSize: 32)
-    //                   ),
-    //                   onPressed: () {
-    //                     starTrekNav(context);
-    //                   },
-    //                 ),
-    //                 const SizedBox(width: 12),
-    //                 TextButton(
-    //                   child: const Text('Season 6'),
-    //                   style: TextButton.styleFrom(
-    //                     textStyle: TextStyle(fontSize: 32)
-    //                   ),
-    //                   onPressed: () {
-    //                     starTrekNav(context);
-    //                   },
-    //                 ),
-    //                 const SizedBox(width: 12),
-    //                 TextButton(
-    //                   child: const Text('Season 7'),
-    //                   style: TextButton.styleFrom(
-    //                     textStyle: TextStyle(fontSize: 32)
-    //                   ),
-    //                   onPressed: () {
-    //                     starTrekNav(context);
-    //                   },
-    //                 ),
-    //                 const SizedBox(width: 12),
-
-    //               ]
-    //             )),
-    //           ]),
-    //         )
-    //     ),
-    //   )
-    // ),
-
-
+    
+    
 
 
   ]);
---
+++ lib/tvshows/alteredcarboncard.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void alteredCarbonNav(context) {
+  Navigator.pushNamed(context, '/Junk');
+}
+
+class AlteredCarbonCard extends StatelessWidget {
+  @override
+    Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        color: Colors.purple[900],
+        child: InkWell(
+          splashColor: Colors.red.withAlpha(15),
+          onTap: () {
+            print('Card tapped.');
+          },
+          child: SizedBox(
+            width: 775.0,
+            height: 290.0,
+            child: Row(
+              children: <Widget>[
+                Image.asset(
+                  'images/alteredcarbon.jpg',
+                  fit: BoxFit.contain,
+                    height: 355.5,
+                    width: 200.0,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0.0, 100.0, 0.0, 0.0),
+
+                  child: Column(
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text('Season 1'),
+                      style: TextButton.styleFrom(
+                        textStyle: TextStyle(fontSize: 32, color: Colors.white)
+                      ),
+                      onPressed: () {
+                        alteredCarbonNav(context);
+                      },
+                    ),
+                    const SizedBox(width: 12),
+                    TextButton(
+                      child: const Text('Season 2'),
+                      style: TextButton.styleFrom(
+                        textStyle: TextStyle(fontSize: 32)
+                      ),
+                      onPressed: () {
+                        alteredCarbonNav(context);
+                      },
+                    ),
+                    const SizedBox(width: 12),
+                  ]
+                ))),
+              ]),
+            )
+        ),
+      )
+    );
+
+  }
+}
