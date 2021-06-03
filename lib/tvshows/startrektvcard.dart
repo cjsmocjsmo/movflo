e1fb18a06e9daae7d4a70be9e8c7b711645f87be
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-06-03 04:40:04 -0700

    added star trek

--- lib/tvshows/startrektvcard.dart
+++ lib/tvshows/startrektvcard.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
-
-void starTrekNav(context) {
-  // Navigator.pushNamed(context, '/MyStatefulWidget');
-  Navigator.pushNamed(context, '/Junk');
-}
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class StarTrekTVCard extends StatelessWidget {
+
+  final String api1Url = "http://192.168.0.42:8888/intSTTV?season=01";
+  
+
+  Future<List<dynamic>> fetchStarTrekSeason1() async {
+    
+      var result = await http.get(Uri.parse(api1Url));
+      return json.decode(result.body);
+    
+  }
+
+  final String api2Url = "http://192.168.0.42:8888/intSTTV?season=02";
+
+  Future<List<dynamic>> fetchStarTrekSeason2() async {
+    
+      var result = await http.get(Uri.parse(api2Url));
+      return json.decode(result.body);
+    
+  }
+
+  final String api3Url = "http://192.168.0.42:8888/intSTTV?season=03";
+
+  Future<List<dynamic>> fetchStarTrekSeason3() async {
+    
+      var result = await http.get(Uri.parse(api3Url));
+      return json.decode(result.body);
+    
+  }
+
+  Future<void> playEpi(playURL) async {
+
+    try {
+      var resultPlay = await http.get(Uri.parse(playURL));
+      return json.decode(resultPlay.body);
+    } catch (e) {
+      print(e);
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -43,18 +80,57 @@ class StarTrekTVCard extends StatelessWidget {
                           MaterialPageRoute<void>(builder: (BuildContext context) {
                             return Scaffold(
                               appBar: AppBar(
-                                title: Text("Star Trek TV"),
+                                title: Text("Star Trek"),
                                 backgroundColor: Colors.lightGreen[900],
                               ),
-                              body: const Center(
-                                child: Text(
-                                  'This is Star Trek TV episode list',
-                                  style: TextStyle(fontSize: 24),
-                                ),
+                              body: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.lightGreenAccent.shade400,
                               ),
-                            );
-                          }
-                        ));
+                              
+                              child: Center(
+                                child: FutureBuilder<List<dynamic>>(
+                                  future: fetchStarTrekSeason1(),
+                                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                                    if(snapshot.hasData){
+                                      print(snapshot.data[0]['tvfspath']);
+                                      return ListView.builder(
+                                        padding: const EdgeInsets.all(8),
+                                        itemCount: snapshot.data.length,
+                                        itemBuilder: (BuildContext context, int index) {
+                                          return GestureDetector(
+                                            onTap: () {
+                                              print(snapshot.data[index]);
+                                              String dirp = "/media/pi/PiTB/media/TVShows";
+                                              String ap = dirp + snapshot.data[index]['tvfspath'];
+                                              print(ap);
+                                              final String apiPU = "http://192.168.0.42:8181/OmxplayerPlayMediaReact?medPath=${ap}";
+                                              playEpi(apiPU);
+                                              Navigator.pop(context);
+                                            },
+                                            child: Container(
+                                              height: 50,
+                                              color: Colors.amber[600],
+                                              child: Center(
+                                                child: Text(
+                                                  '${snapshot.data[index]['title']}',
+                                                  style: TextStyle(fontSize: 32, color: Colors.black),
+                                                ),
+                                              ),
+                                            )
+                                          );
+                                        }
+                                      );
+                                    } else {
+                                      return Text("OOOOOOOH FUCK");
+                                    }
+                                    return CircularProgressIndicator();
+                                  }
+                                ),
+                            )));
+                            }
+                          )
+                        );
                       },
                     ),
                     const SizedBox(width: 12),
@@ -64,50 +140,200 @@ class StarTrekTVCard extends StatelessWidget {
                         textStyle: TextStyle(fontSize: 32)
                       ),
                       onPressed: () {
-                        Navigator.push(context,
+                         Navigator.push(context,
                           MaterialPageRoute<void>(builder: (BuildContext context) {
                             return Scaffold(
                               appBar: AppBar(
-                                title: Text("Star Trek TV"),
+                                title: Text("Star Trek"),
                                 backgroundColor: Colors.lightGreen[900],
                               ),
-                              body: const Center(
-                                child: Text(
-                                  'This is Star Trek TV episode list',
-                                  style: TextStyle(fontSize: 24),
-                                ),
+                              body: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.lightGreenAccent.shade400,
                               ),
-                            );
-                          }
-                        ));
-                      },
-                    ),
-                    const SizedBox(width: 12),
+                              
+                              child: Center(
+                                child: FutureBuilder<List<dynamic>>(
+                                  future: fetchStarTrekSeason2(),
+                                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                                    if(snapshot.hasData){
+                                      print(snapshot.data[0]['tvfspath']);
+                                      return ListView.builder(
+                                        padding: const EdgeInsets.all(8),
+                                        itemCount: snapshot.data.length,
+                                        itemBuilder: (BuildContext context, int index) {
+                                          return GestureDetector(
+                                            onTap: () {
+                                              String dirp = "/media/pi/PiTB/media/TVShows";
+                                              String ap = dirp + snapshot.data[index]['tvfspath'];
+                                              print(ap);
+                                              final String apiPU = "http://192.168.0.42:8181/OmxplayerPlayMediaReact?medPath=${ap}";
+                                              print(apiPU);
+                                              playEpi(apiPU);
+                                              Navigator.pop(context);
+                                            },
+                                            child: Container(
+                                              height: 50,
+                                              color: Colors.amber[600],
+                                              child: Center(
+                                                child: Text(
+                                                  '${snapshot.data[index]['title']}',
+                                                  style: TextStyle(fontSize: 32, color: Colors.black),
+                                                ),
+                                              ),
+                                            )
+                                          );
+                                        }
+                                      );
+                                    } else {
+                                      return Text("OOOOOOOH FUCK");
+                                    }
+                                    return CircularProgressIndicator();
+                                  }
+                                ),
+                            ))
+
+                          );
+                    }));
+                    }),
+                     const SizedBox(width: 12),
                     TextButton(
                       child: const Text('Season 3'),
                       style: TextButton.styleFrom(
                         textStyle: TextStyle(fontSize: 32)
                       ),
                       onPressed: () {
-                        Navigator.push(context,
+                         Navigator.push(context,
                           MaterialPageRoute<void>(builder: (BuildContext context) {
                             return Scaffold(
                               appBar: AppBar(
-                                title: Text("Star Trek TV"),
+                                title: Text("Star Trek"),
                                 backgroundColor: Colors.lightGreen[900],
                               ),
-                              body: const Center(
-                                child: Text(
-                                  'This is Star Trek TV episode list',
-                                  style: TextStyle(fontSize: 24),
-                                ),
+                              body: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.lightGreenAccent.shade400,
                               ),
-                            );
-                          }
-                        ));
-                      },
-                    ),
-                    const SizedBox(width: 12),
+                              
+                              child: Center(
+                                child: FutureBuilder<List<dynamic>>(
+                                  future: fetchStarTrekSeason2(),
+                                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                                    if(snapshot.hasData){
+                                      print(snapshot.data[0]['tvfspath']);
+                                      return ListView.builder(
+                                        padding: const EdgeInsets.all(8),
+                                        itemCount: snapshot.data.length,
+                                        itemBuilder: (BuildContext context, int index) {
+                                          return GestureDetector(
+                                            onTap: () {
+                                              String dirp = "/media/pi/PiTB/media/TVShows";
+                                              String ap = dirp + snapshot.data[index]['tvfspath'];
+                                              print(ap);
+                                              final String apiPU = "http://192.168.0.42:8181/OmxplayerPlayMediaReact?medPath=${ap}";
+                                              print(apiPU);
+                                              playEpi(apiPU);
+                                              Navigator.pop(context);
+                                            },
+                                            child: Container(
+                                              height: 50,
+                                              color: Colors.amber[600],
+                                              child: Center(
+                                                child: Text(
+                                                  '${snapshot.data[index]['title']}',
+                                                  style: TextStyle(fontSize: 32, color: Colors.black),
+                                                ),
+                                              ),
+                                            )
+                                          );
+                                        }
+                                      );
+                                    } else if (snapshot.hasError) {
+                                      return Text("${snapshot.error}");
+                                    }
+                                    return CircularProgressIndicator();
+                                  }
+                                ),
+                            ))
+
+                          );
+                    }));
+                    })
+                    // TextButton(
+                    //   child: const Text('Season 1'),
+                    //   style: TextButton.styleFrom(
+                    //     textStyle: TextStyle(fontSize: 32, color: Colors.white)
+                    //   ),
+                    //   onPressed: () {
+                    //     Navigator.push(context,
+                    //       MaterialPageRoute<void>(builder: (BuildContext context) {
+                    //         return Scaffold(
+                    //           appBar: AppBar(
+                    //             title: Text("Star Trek TV"),
+                    //             backgroundColor: Colors.lightGreen[900],
+                    //           ),
+                    //           body: const Center(
+                    //             child: Text(
+                    //               'This is Star Trek TV episode list',
+                    //               style: TextStyle(fontSize: 24),
+                    //             ),
+                    //           ),
+                    //         );
+                    //       }
+                    //     ));
+                    //   },
+                    // ),
+                    // const SizedBox(width: 12),
+                    // TextButton(
+                    //   child: const Text('Season 2'),
+                    //   style: TextButton.styleFrom(
+                    //     textStyle: TextStyle(fontSize: 32)
+                    //   ),
+                    //   onPressed: () {
+                    //     Navigator.push(context,
+                    //       MaterialPageRoute<void>(builder: (BuildContext context) {
+                    //         return Scaffold(
+                    //           appBar: AppBar(
+                    //             title: Text("Star Trek TV"),
+                    //             backgroundColor: Colors.lightGreen[900],
+                    //           ),
+                    //           body: const Center(
+                    //             child: Text(
+                    //               'This is Star Trek TV episode list',
+                    //               style: TextStyle(fontSize: 24),
+                    //             ),
+                    //           ),
+                    //         );
+                    //       }
+                    //     ));
+                    //   },
+                    // ),
+                    // const SizedBox(width: 12),
+                    // TextButton(
+                    //   child: const Text('Season 3'),
+                    //   style: TextButton.styleFrom(
+                    //     textStyle: TextStyle(fontSize: 32)
+                    //   ),
+                    //   onPressed: () {
+                    //     Navigator.push(context,
+                    //       MaterialPageRoute<void>(builder: (BuildContext context) {
+                    //         return Scaffold(
+                    //           appBar: AppBar(
+                    //             title: Text("Star Trek TV"),
+                    //             backgroundColor: Colors.lightGreen[900],
+                    //           ),
+                    //           body: const Center(
+                    //             child: Text(
+                    //               'This is Star Trek TV episode list',
+                    //               style: TextStyle(fontSize: 24),
+                    //             ),
+                    //           ),
+                    //         );
+                    //       }
+                    //     ));
+                    //   },
+                    // ),
+                    // const SizedBox(width: 12),
                   ]
                 ))),
               ]),
