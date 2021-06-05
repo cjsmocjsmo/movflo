1ace5f416ad3ebe95cf39be242c9f82fcb6ef18e
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-06-04 23:47:34 -0700

    added docker compose

--- movflorun.sh
+++ movflorun.sh
@@ -1,2 +1,7 @@
+flutter build web && \
+snapcraft --use-lxd && \
+
+
+
 docker build -t movflohttpdimg . && \
-docker run -dit --name movflo -p 7777:80 movflohttpdimg
+docker run -dit --name movflo -p 7777:80 movflohttpdimg && \
