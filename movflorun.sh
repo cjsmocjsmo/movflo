54497e75034797317ad5ff7b31f8518aa10e059d
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-06-07 10:47:44 -0700

    update

--- movflorun.sh
+++ movflorun.sh
@@ -1,14 +1,11 @@
 flutter clean && \
 flutter build web && \
-docker stop movflo && \
-docker rm movflo && \
+# docker stop movflo && \
+# docker rm movflo && \
 docker build -t movflohttpdimg . && \
 docker-compose up -d --build && \
 # docker run -dit --name movflo -p 7777:80 movflohttpdimg && \
 
 snapcraft --use-lxd && \
 sudo snap install --dangerous movflo*snap
-
-
-
 
