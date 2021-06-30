b1ccfb9b045ab22912438d389b149efeeb6a7323
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-06-30 15:45:45 -0700

    refactored picard

--- lib/tvshows/picardcard.dart
+++ lib/tvshows/picardcard.dart
@@ -3,35 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class PicardCard extends StatelessWidget {
-
-  final String api1Url = "http://192.168.0.42:8888/intPicard?season=01";
   
-
-  Future<List<dynamic>> fetchPicardSeason1() async {
-    
-      var result = await http.get(Uri.parse(api1Url));
-      return json.decode(result.body);
-    
-  }
-
-  final String api2Url = "http://192.168.0.42:8888/intPicard?season=02";
-
-  Future<List<dynamic>> fetchPicardSeason2() async {
-    
-      var result = await http.get(Uri.parse(api2Url));
-      return json.decode(result.body);
-    
-  }
-
-  Future<void> playEpi(playURL) async {
-
-    try {
-      var resultPlay = await http.get(Uri.parse(playURL));
-      return json.decode(resultPlay.body);
-    } catch (e) {
-      print(e);
-    }
-  }
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -56,12 +28,11 @@ class PicardCard extends StatelessWidget {
                 Expanded(
                   child: Padding(
                     padding: EdgeInsets.fromLTRB(0.0, 80.0, 0.0, 0.0),
-
-                  child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
-                      child: SizedBox.fromSize(
+                    child: Column(
+                      children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                        child: SizedBox.fromSize(
                           size: Size(66, 66), // button width and height
                           child: ClipOval(
                             child: Material(
@@ -69,72 +40,9 @@ class PicardCard extends StatelessWidget {
                               child: InkWell(
                                 splashColor: Colors.green, // splash color
                                 onTap: () {
-                                  Navigator.push(context, MaterialPageRoute<void>(
-                                    builder: (BuildContext context) {
-                                    return Scaffold(
-                                      appBar: AppBar(
-                                        title: Text("Picard"),
-                                        backgroundColor: Colors.lightGreen[900],
-                                      ),
-                                      body: Container(
-                                        decoration: BoxDecoration(
-                                          color: Colors.lightGreenAccent.shade400,
-                                        ),
-                                        child: Center(
-                                          child:
-                                            FutureBuilder<List<dynamic>>(
-                                              future: fetchPicardSeason1(),
-                                              builder: (BuildContext context,AsyncSnapshot snapshot) {
-                                                if (snapshot.hasData) {
-                                                  return ListView.builder(
-                                                    padding: const EdgeInsets.all(8),
-                                                    itemCount: snapshot.data.length,
-                                                    itemBuilder: (BuildContext context, int index) {
-                                                      return GestureDetector(
-                                                        onTap: () {
-                                                          String dirp = "/media/pi/PiTB/media/TVShows";
-                                                          String ap = dirp + snapshot.data[index]["tvfspath"];
-                                                          final String apiPU = "http://192.168.0.42:8181/OmxplayerPlayMediaReact?medPath=" + ap;
-                                                          playEpi(apiPU);
-                                                          Navigator.pop(context);
-                                                        },
-                                                        child: 
-
-                                                        Container(
-                                                          height: 75,
-                                                          color: Colors.amber[600],
-                                                          child:Center(
-                                                            child: Text(
-                                                              '${snapshot.data[index]['title']}',
-                                                              style: TextStyle(fontSize: 32, color: Colors.black),
-                                                            ),
-                                                          ),
-                                                        ),
-                                                      );
-                                                    });
-                                                } else {
-                                                  return CircularProgressIndicator();
-                                                }
-                                                
-                                              }),
-                                        )));
-                                  }));
-                      
+                                  _picard(context, '1');
                                 }, // button pressed
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    // Icon(Icons.call), // icon
-                                    Text(
-                                      "1",
-                                      style: TextStyle(
-                                        fontFamily: "Gothic",
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 22, 
-                                        color: Colors.black),
-                                    ), // text
-                                  ],
-                                ),
+                                child: _picardButtonColumn('1')
                               ),
                             ),
                           ),
@@ -150,83 +58,117 @@ class PicardCard extends StatelessWidget {
                               child: InkWell(
                                 splashColor: Colors.green, // splash color
                                 onTap: () {
-                                  Navigator.push(context, MaterialPageRoute<void>(
-                                    builder: (BuildContext context) {
-                                    return Scaffold(
-                                      appBar: AppBar(
-                                        title: Text("Picard"),
-                                        backgroundColor: Colors.lightGreen[900],
-                                      ),
-                                      body: Container(
-                                        decoration: BoxDecoration(
-                                          color: Colors.lightGreenAccent.shade400,
-                                        ),
-                                        child: Center(
-                                          child:
-                                            FutureBuilder<List<dynamic>>(
-                                              future: fetchPicardSeason2(),
-                                              builder: (BuildContext context,AsyncSnapshot snapshot) {
-                                                if (snapshot.hasData) {
-                                                  return ListView.builder(
-                                                    padding: const EdgeInsets.all(8),
-                                                    itemCount: snapshot.data.length,
-                                                    itemBuilder: (BuildContext context, int index) {
-                                                      return GestureDetector(
-                                                        onTap: () {
-                                                          String dirp = "/media/pi/PiTB/media/TVShows";
-                                                          String ap = dirp + snapshot.data[index]["tvfspath"];
-                                                          final String apiPU = "http://192.168.0.42:8181/OmxplayerPlayMediaReact?medPath=" + ap;
-                                                          playEpi(apiPU);
-                                                          Navigator.pop(context);
-                                                        },
-                                                        child: 
-
-                                                        Container(
-                                                          height: 75,
-                                                          color: Colors.amber[600],
-                                                          child:Center(
-                                                            child: Text(
-                                                              '${snapshot.data[index]['title']}',
-                                                              style: TextStyle(fontSize: 32, color: Colors.black),
-                                                            ),
-                                                          ),
-                                                        ),
-                                                      );
-                                                    });
-                                                } else {
-                                                  return CircularProgressIndicator();
-                                                }
-                                                
-                                              }),
-                                        )));
-                                  }));
-                      
+                                  _picard(context, '2');
                                 }, // button pressed
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    // Icon(Icons.call), // icon
-                                    Text(
-                                      "2",
-                                      style: TextStyle(
-                                        fontFamily: "Gothic",
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 22, 
-                                        color: Colors.black),
-                                    ), // text
-                                  ],
-                                ),
+                                child: _picardButtonColumn('2')
                               ),
                             ),
                           ),
                         ),
                       ),
-                  ]
-                ))),
-              ]),
-            )
+                  ]),
+                  ),
+                ),
+              ]
+            ),
+          ),
         ),
-      )
+      ),
     );
   }
+}
+
+Future<void> playEpi(playURL) async {
+  try {
+    var resultPlay = await http.get(Uri.parse(playURL));
+    return json.decode(resultPlay.body);
+  } catch (e) {
+    print(e);
+  }
+}
+
+_picard(BuildContext context, String season_num) {
+
+  Future<List<dynamic>> fetchPicard() async {
+
+    final String api1Url = "http://192.168.0.42:8888/intPicard?season=01";
+    // final String api2Url = "http://192.168.0.42:8888/intPicard?season=02";
+
+    if (season_num == '1') {
+      var result = await http.get(Uri.parse(api1Url));
+      return json.decode(result.body);
+    } else {
+      var result = await http.get(Uri.parse(api1Url));
+      return json.decode(result.body);
+    }
+  }
+
+  return Navigator.push(context, MaterialPageRoute<void>(
+    builder: (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Picard"),
+        backgroundColor: Colors.lightGreen[900],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.lightGreenAccent.shade400,
+        ),
+        child: Center(
+          child:
+            FutureBuilder<List<dynamic>>(
+              future: fetchPicard(),
+              builder: (BuildContext context,AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return GestureDetector(
+                        onTap: () {
+                          String dirp = "/media/pi/PiTB/media/TVShows";
+                          String ap = dirp + snapshot.data[index]["tvfspath"];
+                          final String apiPU = "http://192.168.0.42:8181/OmxplayerPlayMediaReact?medPath=" + ap;
+                          playEpi(apiPU);
+                          Navigator.pop(context);
+                        },
+                        child: 
+                        Container(
+                          height: 75,
+                          color: Colors.amber[600],
+                          child:Center(
+                            child: Text(
+                              '${snapshot.data[index]['title']}',
+                              style: TextStyle(fontSize: 32, color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      );
+                    });
+                } else {
+                  return CircularProgressIndicator();
+                }}
+              ),
+            ),
+          ),
+        );
+      }
+    ),
+  );
+}
+
+_picardButtonColumn(String episode) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        episode,
+        style: TextStyle(
+          fontFamily: "Gothic",
+          fontWeight: FontWeight.bold,
+          fontSize: 22, 
+          color: Colors.black),
+      ), // text
+    ],
+  );
 }
