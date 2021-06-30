d26ac59b5313ca4e943361736859f350f70ea896
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-06-30 15:26:05 -0700

    refactored orville

--- lib/tvshows/orvillecard.dart
+++ lib/tvshows/orvillecard.dart
@@ -3,45 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class OrvilleCard extends StatelessWidget {
-  
-  final String api1Url = "http://192.168.0.42:8888/intOrville?season=01";
-  
 
-  Future<List<dynamic>> fetchOrvilleSeason1() async {
-    
-      var result = await http.get(Uri.parse(api1Url));
-      return json.decode(result.body);
-    
-  }
-
-  final String api2Url = "http://192.168.0.42:8888/intOrville?season=02";
-
-  Future<List<dynamic>> fetchOrvilleSeason2() async {
-    
-      var result = await http.get(Uri.parse(api2Url));
-      return json.decode(result.body);
-    
-  }
-
-  final String api3Url = "http://192.168.0.42:8888/intOrville?season=03";
-
-  Future<List<dynamic>> fetchOrvilleSeason3() async {
-    
-      var result = await http.get(Uri.parse(api3Url));
-      return json.decode(result.body);
-    
-  }
-
-  Future<void> playEpi(playURL) async {
-
-    try {
-      var resultPlay = await http.get(Uri.parse(playURL));
-      return json.decode(resultPlay.body);
-    } catch (e) {
-      print(e);
-    }
-
-  }
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -66,7 +28,6 @@ class OrvilleCard extends StatelessWidget {
                 Expanded(
                   child: Padding(
                     padding: EdgeInsets.fromLTRB(0.0, 25.0, 0.0, 0.0),
-
                   child: Column(
                   children: <Widget>[
                     Padding(
@@ -79,72 +40,9 @@ class OrvilleCard extends StatelessWidget {
                               child: InkWell(
                                 splashColor: Colors.green, // splash color
                                 onTap: () {
-                                  Navigator.push(context, MaterialPageRoute<void>(
-                                    builder: (BuildContext context) {
-                                    return Scaffold(
-                                      appBar: AppBar(
-                                        title: Text("Orville"),
-                                        backgroundColor: Colors.lightGreen[900],
-                                      ),
-                                      body: Container(
-                                        decoration: BoxDecoration(
-                                          color: Colors.lightGreenAccent.shade400,
-                                        ),
-                                        child: Center(
-                                          child:
-                                            FutureBuilder<List<dynamic>>(
-                                              future: fetchOrvilleSeason1(),
-                                              builder: (BuildContext context,AsyncSnapshot snapshot) {
-                                                if (snapshot.hasData) {
-                                                  return ListView.builder(
-                                                    padding: const EdgeInsets.all(8),
-                                                    itemCount: snapshot.data.length,
-                                                    itemBuilder: (BuildContext context, int index) {
-                                                      return GestureDetector(
-                                                        onTap: () {
-                                                          String dirp = "/media/pi/PiTB/media/TVShows";
-                                                          String ap = dirp + snapshot.data[index]["tvfspath"];
-                                                          final String apiPU = "http://192.168.0.42:8181/OmxplayerPlayMediaReact?medPath=" + ap;
-                                                          playEpi(apiPU);
-                                                          Navigator.pop(context);
-                                                        },
-                                                        child: 
-
-                                                        Container(
-                                                          height: 75,
-                                                          color: Colors.amber[600],
-                                                          child:Center(
-                                                            child: Text(
-                                                              '${snapshot.data[index]['title']}',
-                                                              style: TextStyle(fontSize: 32, color: Colors.black),
-                                                            ),
-                                                          ),
-                                                        ),
-                                                      );
-                                                    });
-                                                } else {
-                                                  return CircularProgressIndicator();
-                                                }
-                                                
-                                              }),
-                                        )));
-                                  }));
-                      
+                                  _orville(context, '1');
                                 }, // button pressed
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    // Icon(Icons.call), // icon
-                                    Text(
-                                      "1",
-                                      style: TextStyle(
-                                        fontFamily: "Gothic",
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 22, 
-                                        color: Colors.black),
-                                    ), // text
-                                  ],
-                                ),
+                                child: _orvilleButtonColumn('1')
                               ),
                             ),
                           ),
@@ -160,72 +58,9 @@ class OrvilleCard extends StatelessWidget {
                               child: InkWell(
                                 splashColor: Colors.green, // splash color
                                 onTap: () {
-                                  Navigator.push(context, MaterialPageRoute<void>(
-                                    builder: (BuildContext context) {
-                                    return Scaffold(
-                                      appBar: AppBar(
-                                        title: Text("Orville"),
-                                        backgroundColor: Colors.lightGreen[900],
-                                      ),
-                                      body: Container(
-                                        decoration: BoxDecoration(
-                                          color: Colors.lightGreenAccent.shade400,
-                                        ),
-                                        child: Center(
-                                          child:
-                                            FutureBuilder<List<dynamic>>(
-                                              future: fetchOrvilleSeason2(),
-                                              builder: (BuildContext context,AsyncSnapshot snapshot) {
-                                                if (snapshot.hasData) {
-                                                  return ListView.builder(
-                                                    padding: const EdgeInsets.all(8),
-                                                    itemCount: snapshot.data.length,
-                                                    itemBuilder: (BuildContext context, int index) {
-                                                      return GestureDetector(
-                                                        onTap: () {
-                                                          String dirp = "/media/pi/PiTB/media/TVShows";
-                                                          String ap = dirp + snapshot.data[index]["tvfspath"];
-                                                          final String apiPU = "http://192.168.0.42:8181/OmxplayerPlayMediaReact?medPath=" + ap;
-                                                          playEpi(apiPU);
-                                                          Navigator.pop(context);
-                                                        },
-                                                        child: 
-
-                                                        Container(
-                                                          height: 75,
-                                                          color: Colors.amber[600],
-                                                          child:Center(
-                                                            child: Text(
-                                                              '${snapshot.data[index]['title']}',
-                                                              style: TextStyle(fontSize: 32, color: Colors.black),
-                                                            ),
-                                                          ),
-                                                        ),
-                                                      );
-                                                    });
-                                                } else {
-                                                  return CircularProgressIndicator();
-                                                }
-                                                
-                                              }),
-                                        )));
-                                  }));
-                      
+                                  _orville(context, '2');
                                 }, // button pressed
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    // Icon(Icons.call), // icon
-                                    Text(
-                                      "2",
-                                      style: TextStyle(
-                                        fontFamily: "Gothic",
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 22, 
-                                        color: Colors.black),
-                                    ), // text
-                                  ],
-                                ),
+                                child: _orvilleButtonColumn('2')
                               ),
                             ),
                           ),
@@ -241,83 +76,128 @@ class OrvilleCard extends StatelessWidget {
                               child: InkWell(
                                 splashColor: Colors.green, // splash color
                                 onTap: () {
-                                  Navigator.push(context, MaterialPageRoute<void>(
-                                    builder: (BuildContext context) {
-                                    return Scaffold(
-                                      appBar: AppBar(
-                                        title: Text("Orville"),
-                                        backgroundColor: Colors.lightGreen[900],
-                                      ),
-                                      body: Container(
-                                        decoration: BoxDecoration(
-                                          color: Colors.lightGreenAccent.shade400,
-                                        ),
-                                        child: Center(
-                                          child:
-                                            FutureBuilder<List<dynamic>>(
-                                              future: fetchOrvilleSeason3(),
-                                              builder: (BuildContext context,AsyncSnapshot snapshot) {
-                                                if (snapshot.hasData) {
-                                                  return ListView.builder(
-                                                    padding: const EdgeInsets.all(8),
-                                                    itemCount: snapshot.data.length,
-                                                    itemBuilder: (BuildContext context, int index) {
-                                                      return GestureDetector(
-                                                        onTap: () {
-                                                          String dirp = "/media/pi/PiTB/media/TVShows";
-                                                          String ap = dirp + snapshot.data[index]["tvfspath"];
-                                                          final String apiPU = "http://192.168.0.42:8181/OmxplayerPlayMediaReact?medPath=" + ap;
-                                                          playEpi(apiPU);
-                                                          Navigator.pop(context);
-                                                        },
-                                                        child: 
-
-                                                        Container(
-                                                          height: 75,
-                                                          color: Colors.amber[600],
-                                                          child:Center(
-                                                            child: Text(
-                                                              '${snapshot.data[index]['title']}',
-                                                              style: TextStyle(fontSize: 32, color: Colors.black),
-                                                            ),
-                                                          ),
-                                                        ),
-                                                      );
-                                                    });
-                                                } else {
-                                                  return CircularProgressIndicator();
-                                                }
-                                                
-                                              }),
-                                        )));
-                                  }));
-                      
+                                  _orville(context, '3');
                                 }, // button pressed
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    // Icon(Icons.call), // icon
-                                    Text(
-                                      "3",
-                                      style: TextStyle(
-                                        fontFamily: "Gothic",
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 22, 
-                                        color: Colors.black),
-                                    ), // text
-                                  ],
-                                ),
+                                child: _orvilleButtonColumn('3')
                               ),
                             ),
                           ),
                         ),
                       ),
-                  ]
-                ))),
-              ]),
-            )
+                    ]),
+                  ),
+                ),
+              ]
+            ),
+          ),
         ),
-      )
+      ),
     );
   }
+}
+
+Future<void> playEpi(playURL) async {
+  try {
+    var resultPlay = await http.get(Uri.parse(playURL));
+    return json.decode(resultPlay.body);
+  } catch (e) {
+    print(e);
+  }
+}
+
+_orville(BuildContext context, String season_num) {
+  
+  
+
+  Future<List<dynamic>> fetchOrvilleSeason1() async {
+
+    final String api1Url = "http://192.168.0.42:8888/intOrville?season=01";
+    final String api2Url = "http://192.168.0.42:8888/intOrville?season=02";
+    if (season_num == '1') {
+      var result = await http.get(Uri.parse(api1Url));
+      return json.decode(result.body);
+    } else {
+      var result = await http.get(Uri.parse(api2Url));
+      return json.decode(result.body);
+    }
+
+    // final String api3Url = "http://192.168.0.42:8888/intOrville?season=03";
+    
+    // if (season_num == '1') {
+    //   var result = await http.get(Uri.parse(api1Url));
+    //   return json.decode(result.body);
+    // } else if (season_num == '2') {
+    //   var result = await http.get(Uri.parse(api2Url));
+    //   return json.decode(result.body);
+    // } else {
+    //   var result = await http.get(Uri.parse(api3Url));
+    //   return json.decode(result.body);
+    // }
+    
+  }
+  return Navigator.push(context, MaterialPageRoute<void>(
+    builder: (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Orville"),
+        backgroundColor: Colors.lightGreen[900],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.lightGreenAccent.shade400,
+        ),
+        child: Center(
+          child:
+            FutureBuilder<List<dynamic>>(
+              future: fetchOrvilleSeason1(),
+              builder: (BuildContext context,AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return GestureDetector(
+                        onTap: () {
+                          String dirp = "/media/pi/PiTB/media/TVShows";
+                          String ap = dirp + snapshot.data[index]["tvfspath"];
+                          final String apiPU = "http://192.168.0.42:8181/OmxplayerPlayMediaReact?medPath=" + ap;
+                          playEpi(apiPU);
+                          Navigator.pop(context);
+                        },
+                        child: 
+                        Container(
+                          height: 75,
+                          color: Colors.amber[600],
+                          child:Center(
+                            child: Text(
+                              '${snapshot.data[index]['title']}',
+                              style: TextStyle(fontSize: 32, color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      );
+                    });
+                } else {
+                  return CircularProgressIndicator();
+                }
+                
+              }),
+        )));
+  }));
+}
+
+_orvilleButtonColumn(String episode) {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        episode,
+        style: TextStyle(
+          fontFamily: "Gothic",
+          fontWeight: FontWeight.bold,
+          fontSize: 22, 
+          color: Colors.black),
+      ), // text
+    ],
+  );
 }
