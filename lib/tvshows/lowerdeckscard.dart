8ec7ee095e7a67c4a42f6081ef4f20c7f8045677
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-08-14 17:57:17 -0700

    update lowerdecks for new season

--- lib/tvshows/lowerdeckscard.dart
+++ lib/tvshows/lowerdeckscard.dart
@@ -118,10 +118,15 @@ Future<void> playEpi(playURL) async {
 _lowerDecks(BuildContext context, String season_num) {
 
   Future<List<dynamic>> fetchLowerDecks() async {
-    final String api1Url = "http://192.168.0.42:8888/intLowerDecks?season=01";
-    // final String api2Url = "http://192.168.0.42:8888/intLowerDecks?season=02";
+    if (season_num == "1") {
+      final String apiUrl = "http://192.168.0.42:8888/intLowerDecks?season=01";
+    } else if (season_num == "2") {
+      final String apiUrl = "http://192.168.0.42:8888/intLowerDecks?season=02";
+    }
     
-      var result = await http.get(Uri.parse(api1Url));
+    
+    
+      var result = await http.get(Uri.parse(apiUrl));
       return json.decode(result.body);
     
   }
