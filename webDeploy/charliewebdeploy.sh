c2bc9a1c1200ef657bed562610085839df3f7677
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2022-08-07 11:16:03 -0700

    change orderin

--- webDeploy/charliewebdeploy.sh
+++ webDeploy/charliewebdeploy.sh
@@ -2,11 +2,11 @@ rm -rf /media/charliepi/HD/movfloWebDeploy;
 
 mkdir /media/charliepi/HD/movfloWebDeploy;
 
-mkdir /media/charliepi/HD/movfloWebDeploy/movflo;
-
 cp -pvr /media/charliepi/HD/mediacenter/movflo/webDeploy/docker-compose.yml /media/charliepi/HD/movfloWebDeploy;
 
 cp -pvr /media/charliepi/HD/mediacenter/movflo/webDeploy/Dockerfile /media/charliepi/HD/movfloWebDeploy;
+
+mkdir /media/charliepi/HD/movfloWebDeploy/movflo;
 
 flutter clean && flutter build web;
 
